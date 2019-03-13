7957517b5ed092307e3410c1e73af4fb2be1c84b
Sakshya Arora <sakshayarora01111@gmail.com>
2019-03-14 01:09:03 +0530

    Add delete functionality

--- Todo Using Arrays/script.js
+++ Todo Using Arrays/script.js
@@ -6,12 +6,25 @@ while(input !== "quit")
 {
     if(input === "list")
     {
-        console.log(todo);    
+        console.log("**************");
+        for(var i=0;i<todo.length;i++)
+        {
+            console.log(i+ ": " + todo[i]);    
+        }
+        console.log("**************");
+    }
+    else if(input === "delete")
+    {
+        var index = prompt("Enter index of todo to delete");   
+        todo.splice(index,1);
+        console.log("Todo deleted");
     }
     else if(input == "new")
     {
         var task = prompt("Enter a new todo");
         todo.push(task);
+        console.log("Todo added");
     }
     var input = prompt("What would you like to do");
 }
+console.log("OK,YOU QUIT THE APP");
