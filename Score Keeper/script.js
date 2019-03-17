3c0fe3717cb6276d292b65db21d5e8bf49860fcf
Sakshya Arora <sakshayarora01111@gmail.com>
2019-03-18 00:21:18 +0530

    add reset functionality

--- Score Keeper/script.js
+++ Score Keeper/script.js
@@ -4,12 +4,16 @@ var p2 = document.getElementsByClassName('p2')[0];
 var p1display = document.querySelector('.p1display');
 var p2display = document.querySelector('.p2display');
 
+var playingFor = document.querySelector('.playingFor');
+var inputnum = document.querySelector('.totalscores');
+
 var p1Score = 0;
 var p2Score = 0;
 
 var gameOver = false;
 var winningScore = 5;
 
+var resetButton = document.querySelector('.reset');
 
 p1.addEventListener('click',function(){
 	if(!gameOver)
@@ -19,8 +23,10 @@ p1.addEventListener('click',function(){
 			p1Score++;
 			p1display.textContent = p1Score;
 		}
-		if(p1Score === winningScore)
+		if(p1Score === winningScore){
+			p1display.classList.add('winner');
 			gameOver = true;
+		}
 	}
 });
 
@@ -33,6 +39,29 @@ p2.addEventListener('click',function(){
 			p2display.textContent = p2Score;
 		}
 		if(p2Score === winningScore)
+		{	
 			gameOver = true;
+			p2display.classList.add('winner');
+		}
 	}
 });
+
+inputnum.addEventListener('change',function(){
+	playingFor.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
+});
+resetButton.addEventListener('click',function(){
+	reset();
+});
+
+function reset()
+{
+	p1Score = 0;
+	p2Score = 0;
+	p1display.textContent = 0;
+	p2display.textContent = 0;
+	p1display.classList.remove("winner");
+	p2display.classList.remove("winner");
+	gameOver = false;
+}
